460fe148368096b8e72019c955d4980fdf939c7e
trantuvan <trantuvan.kan@gmail.com>
2023-05-29 15:02:05 +0700

    BaseRepository
    Impl of IBaseRepository. A Generic class base TEntity is contraint to be
    a referential type (class type)
    
    Init with:
    + Reference to ResumeDbContext
    + Dbset using ResumeDbContext (table which type is TEntity)

---
+++ ResumeGenerator.Services/Impls/BaseRepository.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResumeGenerator.Core;
+using System.Linq.Expressions;
+
+namespace ResumeGenerator.Services;
+
+public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+{
+    private readonly ResumeDbContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public BaseRepository(ResumeDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<TEntity>();
+    }
+    public void Add(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddRange(IEnumerable<TEntity> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueryable<TEntity> FindAll(Expression<Func<TEntity, bool>>? predicate = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity?> FindByIdAsync(Guid id, CancellationToken? token = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveRange(IEnumerable<TEntity> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SaveChangesAsync(CancellationToken? token = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+}
