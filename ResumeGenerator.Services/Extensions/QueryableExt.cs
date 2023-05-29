c0b0930634930c412f6bcaddeae7edce5ee00505
trantuvan <trantuvan.kan@gmail.com>
2023-05-29 15:43:17 +0700

    Add Extension method on Linq Where allow customize Func predicate

---
+++ ResumeGenerator.Services/Extensions/QueryableExt.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace ResumeGenerator.Services;
+
+public static class QueryableExt
+{
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> query,
+                                           bool condition,
+                                           Expression<Func<T, bool>> predicate)
+    {
+        return condition ? query.Where(predicate) : query;
+    }
+}
