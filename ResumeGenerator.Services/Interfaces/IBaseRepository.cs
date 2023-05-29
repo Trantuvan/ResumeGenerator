22b2c480eb59e5e96a9d0805d684ed90f9e4c8e9
trantuvan <trantuvan.kan@gmail.com>
2023-05-29 14:42:34 +0700

    Defined base action in each service talking to DB

---
+++ ResumeGenerator.Services/Interfaces/IBaseRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace ResumeGenerator.Services;
+
+public interface IBaseRepository<TEntity> where TEntity : class
+{
+    IQueryable<TEntity> FindAll(Expression<Func<TEntity, bool>>? predicate = default);
+    Task<TEntity?> FindByIdAsync(Guid id, CancellationToken? token = default);
+    void Add(TEntity entity);
+    void AddRange(IEnumerable<TEntity> entities);
+    void Update(TEntity entity);
+    void Remove(TEntity entity);
+    void RemoveRange(IEnumerable<TEntity> entities);
+    Task SaveChangesAsync(CancellationToken? token = default);
+}
