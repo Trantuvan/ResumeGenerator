0c4ff0f754b7b83819eb5085854972a30c3e65d7
trantuvan <trantuvan.kan@gmail.com>
2023-05-31 22:50:07 +0700

    defined a type of form to create PersonalDetails

---
+++ ResumeGenerator.API/DTOS/Forms/PersonalDetailsForm.cs
@@ -0,0 +1,15 @@
+﻿namespace ResumeGenerator.API;
+public class PersonalDetailsForm : BaseDTO
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public DateTime? DOB { get; set; } = DateTime.MinValue;
+    public string Gender { get; set; } = string.Empty;
+    public string? Photo { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string? HeadLine { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    public ICollection<AddressDTO> Addresses { get; set; } = Array.Empty<AddressDTO>();
+}
+
