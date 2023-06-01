f8c9b4d505d3351907abcdd138ccf829a6d33a8a
trantuvan <trantuvan.kan@gmail.com>
2023-06-01 20:46:13 +0700

    Drop nullable FKs & switch to M:N

--- ResumeGenerator.Core/Entities/Address.cs
+++ ResumeGenerator.Core/Entities/Address.cs
@@ -6,15 +6,10 @@ public class Address : BaseEntity<Guid>
     public string City { get; set; } = string.Empty;
     public string? PostalCode { get; set; } = string.Empty;
 
-    /*NOTICE: 2 FKs EducationID & WorkplaceID are nullable.
-     * Cascade on Delete is off. 
-     * When Education | Workplace record is deleted. 
-       Address record which depends on parent record above still remains value,
-       just EduId | WorkplaceId (FK) set to null.
-     RESULT: All Address records will be kept -> huge orphan data are being generated (not yet how to dealt with)
-    */
-    public Education? Education { get; set; } = default!;
-    public Workplace? Workplace { get; set; } = default!;
+    public ICollection<Education> Educations { get; set; } = new List<Education>();
+    public ICollection<EducationAddress> EducationAddresses { get; set; } = new List<EducationAddress>();
+    public ICollection<Workplace> Workplaces { get; set; } = new List<Workplace>();
+    public ICollection<WorkplaceAddress> WorkplaceAddresses { get; set; } = new List<WorkplaceAddress>();
     public ICollection<Person> People { get; set; } = new List<Person>();
     public ICollection<PersonAddress> PersonAddresses { get; set; } = new List<PersonAddress>();
 }
